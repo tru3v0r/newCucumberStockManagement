9ed9a2129ca585aea181f66f4328a997e486af95
kungnx <kevinung94@gmail.com>
2021-11-22 14:47:09 +0100

    Added branch with cucumber 6

--- src/test/java/concombre/RunCucumberTest.java
+++ src/test/java/concombre/RunCucumberTest.java
@@ -1,15 +1,11 @@
 package concombre;
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("concombre")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@RunWith(Cucumber.class)
+@CucumberOptions()
 public class RunCucumberTest {
 }
