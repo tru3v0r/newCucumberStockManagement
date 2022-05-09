7d79c2e4539abbc8fe107c03cc199be941868e69
vvenom <vvenom@henix.fr>
2022-05-09 12:37:17 +0200

    Add Exception throws

--- src/test/java/concombre/StepDefinitions.java
+++ src/test/java/concombre/StepDefinitions.java
@@ -12,7 +12,7 @@ import org.opentestfactory.util.ParameterService;
 public class StepDefinitions {
 
     @Then("The subtraction is correct")
-    public void i_ve_at_least_number_products_in_stock(int expected) {
+    public void i_ve_at_least_number_products_in_stock(int expected) throws ParameterException {
         int var1 = ParameterService.INSTANCE.getInt("CUF_VAR1");
         int var2 = ParameterService.INSTANCE.getTestInt("CUF_VAR2");
         int res = ParameterService.INSTANCE.getGlobalInt("CUF_RES");
